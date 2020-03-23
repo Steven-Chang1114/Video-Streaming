943ff84fb281bc4adba0221e9115b1b5e6996092
Steven Chang <1632460272@qq.com>
2020-03-23 22:36:44 +0000

    create header

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,16 +1,18 @@
 import React from "react"
-import {BrowserRouter, HashRouter , Route, Link} from "react-router-dom"
+import {BrowserRouter, HashRouter , Route} from "react-router-dom"
 import StreamCreate from "./streams/StreamCreate"
 import StreamDelete from "./streams/StreamDelete"
 import StreamEdit from "./streams/StreamEdit"
 import StreamList from "./streams/StreamList"
 import StreamShow from "./streams/StreamShow"
+import Header from "./Header"
 
 const App = () => {
     return(
-        <div>
+        <div className = "ui container">
             <HashRouter>
                 <div>
+                    <Header />
                     <Route path = "/" exact component = {StreamList}/>
                     <Route path = "/streams/new" exact component = {StreamCreate}/>
                     <Route path = "/streams/edit" exact component = {StreamEdit }/>
