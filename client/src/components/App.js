f1d5c958d576d314192f04f28b398af543ae601c
Steven Chang <1632460272@qq.com>
2020-03-23 21:51:25 +0000

    set up stream files

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react"
-import {BrowserRouter, Route} from "react-router-dom"
+import {BrowserRouter, HashRouter , Route, Link} from "react-router-dom"
 
 const App = () => {
     return(
