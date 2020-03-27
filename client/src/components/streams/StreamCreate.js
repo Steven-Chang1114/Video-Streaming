918c9da932e1b348e057d2eb587334daee41b71b
Steven Chang <1632460272@qq.com>
2020-03-27 02:19:37 +0000

    submit form

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -5,16 +5,27 @@ class StreanCreate extends React.Component {
 
     renderInput = (formProps) => {
         return(
-            <input {...formProps.input}/>
+            <div className = "field">
+                <label>{formProps.text}</label>
+                <input {...formProps.input}/>
+            </div>
         )
     }
 
+    onSubmit = (formValues) => {
+        console.log(this.props);
+        console.log(formValues)
+    }
+
+    
+
     render(){
         return(
-            <form>
-                <Field name = "title" component = {this.renderInput}/>
-                <Field name = "description" component = {this.renderInput}/>
-            </form>
+            <form onSubmit = {this.props.handleSubmit(this.onSubmit)} className = "ui form">
+                <Field name = "title" text = "Enter title" component = {this.renderInput}/>
+                <Field name = "description" text = "Enter description" component = {this.renderInput}/>
+                <button className = "ui button primary">Submit</button>
+            </form>   
         )
     }
 }
