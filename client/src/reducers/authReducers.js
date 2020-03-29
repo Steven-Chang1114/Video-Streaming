9f0f34654e19b2ed12bb936a6b88623382e0ddb1
Steven Chang <1632460272@qq.com>
2020-03-30 00:52:54 +0100

    create userId

--- client/src/reducers/authReducers.js
+++ client/src/reducers/authReducers.js
@@ -8,9 +8,9 @@ const INIT = {
 export default (state = INIT, atcion) => {
     switch(atcion.type){
         case SIGN_IN:
-            return {...state, isSignedIn: true, id: atcion.payload}
+            return {...state, isSignedIn: true, userId: atcion.payload}
         case SIGN_OUT:
-            return {...state, isSignedIn: false, id: null}
+            return {...state, isSignedIn: false, userId: null}
         default:
             return state
     }
