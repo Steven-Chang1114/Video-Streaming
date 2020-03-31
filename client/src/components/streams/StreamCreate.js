832e76749a7e02c3536d5d07cc10f241017e852e
Steven Chang <1632460272@qq.com>
2020-03-31 15:59:55 +0100

    refractor streamCreate

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,67 +1,25 @@
 import React from "react"
-import {Field, reduxForm} from "redux-form"
 import {connect} from "react-redux"
 import {createStream} from "../../actions"
+import StreamForm from "./StreamForm"
 
 class StreanCreate extends React.Component {
-
-    renderInput = (formProps) => {
-        const hasError = `field ${(formProps.meta.error && formProps.meta.touched) ? "error" : ""}`
-        return(
-            <div className = {hasError}>
-                <label>{formProps.text}</label>
-                <input {...formProps.input} autoComplete = "off"/>
-                {this.renderError(formProps.meta)}
-            </div>
-        )
-    }
-
-    renderError = ({touched, error}) => {
-        if(touched && error){
-            return (
-                <div className = "ui error message">
-                    <div className = "header">{error}</div>
-                </div>           
-            )
-        }
-    }
 
     onSubmit = (formValues) => {
         console.log(formValues)
         this.props.createStream(formValues)
     }
 
-    
-
     render(){
         return(
-            <form onSubmit = {this.props.handleSubmit(this.onSubmit)} className = "ui form error">
-                <Field name = "title" text = "Enter title" component = {this.renderInput}/>
-                <Field name = "description" text = "Enter description" component = {this.renderInput}/>
-                <button className = "ui button primary">Submit</button>
-            </form>   
+            <div>
+                <h3>Create Stream</h3>
+                <StreamForm actionCallback = {this.onSubmit}/>  
+            </div>
         )
     }
 }
 
-const validate = (formValues) => {
-    const error = {}
-    if(!formValues.title){
-        error.title = "Please define Title"
-    }
-
-    if(!formValues.description){
-        error.description = "Please define Description"
-    }
-
-    return error
-}
-
-const formWrapped = reduxForm({
-    form: 'streamCreate',
-    validate
-})(StreanCreate)
-
 export default connect(null, {
     createStream
-})(formWrapped)
+})(StreanCreate)
